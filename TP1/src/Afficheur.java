a0b4d7ec94f3d3bf9f28e61ba710ce1d4e5c96f4
Julien Laurent <digyanoen@orange.fr>
2017-10-26 11:41:16 +0200

    Add real interface

--- TP1/src/Afficheur.java
+++ TP1/src/Afficheur.java
@@ -1,6 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.util.concurrent.ExecutionException;
 
-public class Afficheur implements ObserverGenerateur{
+public class Afficheur extends Application implements ObserverGenerateur{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ressources/index.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
 
 
     public Afficheur() {}
